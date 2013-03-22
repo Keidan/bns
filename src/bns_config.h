e450476e0523ffc560c156ebe5993b71382fae15
Keidan <kuso.fr@gmail.com>
2013-03-22 23:14:12 +0100

    Add config (generated by cmake)

---
+++ src/bns_config.h
@@ -0,0 +1,8 @@
+#ifndef __BNS_CONFIG_H__
+  #define __BNS_CONFIG_H__
+
+  // the configured options and settings for BNS
+  #define BNS_VERSION_MAJOR 2
+  #define BNS_VERSION_MINOR 0
+
+#endif /* __BNS_CONFIG_H__ */
