4f35adbaa8cd9c4c9f10c4458a9544933feef9f5
Keidan <kuso.fr@gmail.com>
2013-03-22 23:12:00 +0100

    Add utils

--- src/bns_utils.c
+++ src/bns_utils.c
@@ -32,11 +32,9 @@
 #include <linux/if_packet.h>
 #include <unistd.h>
 #include "bns_utils.h"
+#include "bns_logger.h"
 
-/* nombre max de chars dans une ligne */
-#define PRINT_HEX_MAX_PER_LINES 16
-
-int bns_utils_prepare_ifaces(struct iface_s *ifaces, int *maxfd, fd_set *rset) {
+int bns_utils_prepare_ifaces(struct iface_s *ifaces, int *maxfd, fd_set *rset, const char iname[IF_NAMESIZE]) {
   int i;
   struct ifreq ifr;
   struct sockaddr_ll sll;
@@ -49,7 +47,7 @@ int bns_utils_prepare_ifaces(struct iface_s *ifaces, int *maxfd, fd_set *rset) {
   /* Liste toutes les cartes reseaux du PC */
   struct if_nameindex *nameindex = if_nameindex();
   if(nameindex == NULL){
-    fprintf(stderr, "if_nameindex: (%d) %s.\n", errno, strerror(errno));
+    logger("if_nameindex: (%d) %s.\n", errno, strerror(errno));
     return -1;
   }
 
@@ -59,7 +57,8 @@ int bns_utils_prepare_ifaces(struct iface_s *ifaces, int *maxfd, fd_set *rset) {
   /* boucle sur les interfaces */
   i = 0; /* init */
   while(1){
-    if(nameindex[i].if_name == NULL) break;
+    if(!nameindex[i].if_name) break;
+    if(strlen(iname) && strncmp(iname, nameindex[i].if_name, IF_NAMESIZE) != 0) continue;
     /* Recuperation du nom qui sera utilise plus bas. */
     name = nameindex[i++].if_name;
 
@@ -68,7 +67,7 @@ int bns_utils_prepare_ifaces(struct iface_s *ifaces, int *maxfd, fd_set *rset) {
     fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if(fd < 0) {
       if_freenameindex(nameindex);
-      fprintf(stderr, "socket failed: (%d) %s.\n", errno, strerror(errno));
+      logger("socket failed: (%d) %s.\n", errno, strerror(errno));
       return -1;
     }
 
@@ -87,7 +86,7 @@ int bns_utils_prepare_ifaces(struct iface_s *ifaces, int *maxfd, fd_set *rset) {
     if((ioctl(fd, SIOCGIFINDEX, &ifr)) == -1) {
       if_freenameindex(nameindex);
       close(fd);
-      fprintf(stderr, "get index failed: (%d) %s.\n", errno, strerror(errno));
+      logger("get index failed: (%d) %s.\n", errno, strerror(errno));
       return -1;
     }
 
@@ -100,7 +99,7 @@ int bns_utils_prepare_ifaces(struct iface_s *ifaces, int *maxfd, fd_set *rset) {
     if((bind(fd, (struct sockaddr *)&sll, sizeof(sll))) == -1) {
       if_freenameindex(nameindex);
       close(fd);
-      fprintf(stderr, "bind failed: (%d) %s.\n", errno, strerror(errno));
+      logger("bind failed: (%d) %s.\n", errno, strerror(errno));
       return -1;
     }
 
@@ -118,7 +117,7 @@ void bns_utils_add_iface(struct iface_s* list, char name[IF_NAMESIZE], int index
   struct iface_s* node;
   node = (struct iface_s*)malloc(sizeof(struct iface_s));
   if(!node) {
-    fprintf(stderr, "if_nameindex: (%d) %s.\n", errno, strerror(errno));
+    logger("if_nameindex: (%d) %s.\n", errno, strerror(errno));
     return;
   }
   /* init + ajout de l'element */
@@ -148,7 +147,7 @@ _Bool bns_utils_device_is_up(int fd, char name[IF_NAMESIZE]) {
   /* demande la liste des flags */
   int ret = ioctl(fd, SIOCGIFFLAGS, &ifr);
   if (ret == -1) {
-    fprintf(stderr, "flags: (%d) %s.\n", errno, strerror(errno));
+    logger("flags: (%d) %s.\n", errno, strerror(errno));
     return ret;
   }
   /* 1 si le flag up est positionne */
@@ -161,28 +160,53 @@ __u32 bns_utils_datas_available(int fd) {
   /* demande le nombre d'octets quipeuvent etre lues */
   int ret = ioctl(fd, FIONREAD, &available);
   if (ret == -1) {
-    fprintf(stderr, "available: (%d) %s.\n", errno, strerror(errno));
-    return ret;
+    logger("available: (%d) %s.\n", errno, strerror(errno));
+    return ret; /* ... */
   }
   return available;
 }
 
 
-void bns_utils_print_hex(char* buffer, int len) {
+int bns_utils_is_ipv4(const char* ip) {    
+  struct in_addr i_addr;
+  int ret = 0;
+  ret = inet_pton(AF_INET, ip, &i_addr);
+  if(ret == 1) return 1;
+  else if(ret == 0) return 0;
+  return -1;
+}
+ 
+int bns_utils_hostname_to_ip(const char *hostname, char* ip) {
+  struct hostent *he;
+  struct in_addr **addr_list;
+  int i;
+  if((he = gethostbyname(hostname)) == NULL) {
+    logger("gethostbyname: (%d) %s.\n", errno, strerror(errno));
+    return -1;
+  }
+  addr_list = (struct in_addr **) he->h_addr_list;
+  for(i = 0; addr_list[i] != NULL; i++) {
+    strcpy(ip , inet_ntoa(*addr_list[i]) );
+    return 0;
+  }
+  return -1;
+}
+
+void bns_utils_print_hex(FILE* std, char* buffer, int len) {
   int i = 0, max = PRINT_HEX_MAX_PER_LINES, loop = len;
   __u8 *p = (__u8 *)buffer;
   char line [max + 3]; /* spaces + \0 */
   memset(line, 0, sizeof(line));
   while(loop--) {
     __u8 c = *(p++);
-    printf("%02x ", c);
+    fprintf(std, "%02x ", c);
     /* uniquement les espaces et les char visibles */
     if(c >= 0x20 && c <= 0x7e) line[i] = c;
     /* sinon on masque avec un '.' */
     else line[i] = 0x2e; /* . */
     /* on passe a la ligne suivante */
     if(i == max) {
-      printf("  %s\n", line);
+      fprintf(std, "  %s\n", line);
       /* re init */
       i = 0;
       memset(line, 0, sizeof(line));
@@ -191,14 +215,27 @@ void bns_utils_print_hex(char* buffer, int len) {
     else i++;
     /* espace a la moitie */
     if(i == max / 2) {
-      printf(" ");
+      fprintf(std, " ");
       line[i++] = 0x20;
     }
   }
   /* Cette etape permet d'aligner 'line'*/
   if(i != 0 && (i < max || i <= len)) {
-    while(i++ <= max) printf("   "); /* comble avec 3 espaces ex: "00 " */
-    printf("  %s\n", line);
+    while(i++ <= max) fprintf(std, "   "); /* comble avec 3 espaces ex: "00 " */
+    fprintf(std, "  %s\n", line);
   }
-  printf("\n");
+  fprintf(std, "\n");
+}
+
+
+const char* bns_utils_long_to_ip(unsigned int v)  {
+  struct in_addr x;
+  x.s_addr = htonl(v);
+  return inet_ntoa(x);
+}
+
+unsigned int bns_utils_ip_to_long(const char* s) {
+  struct sockaddr_in n;
+  inet_aton(s,&n.sin_addr);
+  return ntohl(n.sin_addr.s_addr);
 }
--- src/bns_utils.h
+++ src/bns_utils.h
@@ -30,6 +30,9 @@
   #include <sys/select.h>
   #include "list.h"
 
+  /* nombre max de chars dans une ligne */
+  #define PRINT_HEX_MAX_PER_LINES 16
+
 
   struct iface_s {
     struct list_head list;              /*!< Liste d'interfaces. */
@@ -37,6 +40,22 @@
     int              index;             /*!< Index de la carte. */
     int              fd;                /*!< FD du socket utilise pour les io's/bind/select. */
   };
+
+
+  /**
+   * Test si l'adresse ip est valide.
+   * @param ip[in] Adresse IP.
+   * @return -1 si erreur, 0 si non match, 1 si match.
+   */
+  int bns_utils_is_ipv4(const char* ip);
+
+  /**
+   * Recuperation de l'adresse ip en fonction du nom de host.
+   * @param hostname[in] Nom du host.
+   * @param name[out] Adresse IP.
+   * @return -1 si erreur sinon 0.
+   */
+  int bns_utils_hostname_to_ip(const char *hostname, char* ip);
 
   /**
    * Effectue un test pour savoir si le device est up
@@ -58,16 +77,17 @@
    * @param buffer[in] Packet.
    * @param len[in] Taille du packet.
    */
-  void bns_utils_print_hex(char* buffer, int len);
+  void bns_utils_print_hex(FILE* std, char* buffer, int len);
 
   /**
    * Liste toutes les interfaces et les ajoutent a la liste (IMPORTANT: apres appel de cette methode des sockets sont ouverts).
-   * @param ifaces[in,out] Liste des interfaces.
+   * @param ifaces[in,out] Liste des interfaces (la taille vaut 1 ou 0 si iname n'est pas vide).
    * @param maxfd[in,out] Utilise pour le select.
    * @param rset[in,out] fd_set utilise pour le select.
+   * @param iname[in] Demande la configuration d'une interface.
    * @return -1 en cas d'erreur sinon 0.
    */
-  int bns_utils_prepare_ifaces(struct iface_s *ifaces, int *maxfd, fd_set *rset);
+  int bns_utils_prepare_ifaces(struct iface_s *ifaces, int *maxfd, fd_set *rset, const char iname[IF_NAMESIZE]);
 
   /**
    * Ajout d'un interface a la liste.
@@ -83,5 +103,19 @@
    * @param ifaces[in,out] Liste a vider.
    */
   void bns_utils_clear_ifaces(struct iface_s* ifaces);
+
+  /**
+   * Transforme un long en adresse IP.
+   * @param v[in] long a transformer.
+   * @return L'adresse IP.
+   */
+  const char* bns_utils_long_to_ip(unsigned int v);
+
+  /**
+   * Transforme une adresse IP en long.
+   * @param s[in] IP a transformer.
+   * @return Long.
+   */
+  unsigned int bns_utils_ip_to_long(const char* s);
 
 #endif /* __BNS_UTILS_H__ */
