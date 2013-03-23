efc64da6a43801dad9da2723cfa67afda37072c2
Keidan <kuso.fr@gmail.com>
2013-03-23 11:20:20 +0100

    Add lisence

--- src/bns_config.h
+++ src/bns_config.h
@@ -1,3 +1,25 @@
+/**
+ *******************************************************************************
+ * @file bns_config.h
+ * @author Keidan
+ * @date 03/01/2013
+ * @par Project
+ * bns
+ *
+ * @par Copyright
+ * Copyright 2011-2013 Keidan, all right reserved
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY.
+ *
+ * Licence summary : 
+ *    You can modify and redistribute the sources code and binaries.
+ *    You can send me the bug-fix
+ *
+ * Term of the licence in in the file licence.txt.
+ *
+ *******************************************************************************
+ */
 #ifndef __BNS_CONFIG_H__
   #define __BNS_CONFIG_H__
 
--- src/bns_packet.c
+++ src/bns_packet.c
@@ -1,3 +1,25 @@
+/**
+ *******************************************************************************
+ * @file bns_packet.c
+ * @author Keidan
+ * @date 03/01/2013
+ * @par Project
+ * bns
+ *
+ * @par Copyright
+ * Copyright 2011-2013 Keidan, all right reserved
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY.
+ *
+ * Licence summary : 
+ *    You can modify and redistribute the sources code and binaries.
+ *    You can send me the bug-fix
+ *
+ * Term of the licence in in the file licence.txt.
+ *
+ *******************************************************************************
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
