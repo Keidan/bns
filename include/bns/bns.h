bbeffade5a3b929346fd874061d117a624a16005
Keidan <kuso.fr@gmail.com>
2013-03-25 09:20:23 +0100

    Oups...

---
+++ include/bns/bns.h
@@ -0,0 +1,31 @@
+/**
+ *******************************************************************************
+ * @file bns.h
+ * @author Keidan
+ * @date 03/01/2013
+ * @par Project
+ * bns
+ *
+ * @par Copyright
+ * Copyright 2011-2013 Keidan, all right reserved
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY.
+ *
+ * Licence summary : 
+ *    You can modify and redistribute the sources code and binaries.
+ *    You can send me the bug-fix
+ *
+ * Term of the licence in in the file licence.txt.
+ *
+ *******************************************************************************
+ */
+#ifndef __BNS_H__
+  #define __BNS_H__
+
+  #include <bns/bns_logger.h>
+  #include <bns/bns_packet.h>
+  #include <bns/bns_utils.h>
+
+
+#endif /* __BNS_H__ */
