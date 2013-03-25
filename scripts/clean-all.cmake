8170f4f627809ded58c8856fabbce4fdd53d4a63
Keidan <kuso.fr@gmail.com>
2013-03-25 11:28:01 +0100

    Remove build dirs

--- scripts/clean-all.cmake
+++ scripts/clean-all.cmake
@@ -9,13 +9,14 @@ set(cmake_generated ${CMAKE_BINARY_DIR}/CMakeCache.txt
                     ${CMAKE_BINARY_DIR}/src/binary/cmake_install.cmake  
                     ${CMAKE_BINARY_DIR}/src/binary/Makefile
                     ${CMAKE_BINARY_DIR}/src/binary/CMakeFiles
+                    ${CMAKE_BINARY_DIR}/bin/x86
+                    ${CMAKE_BINARY_DIR}/bin/x86_64
+                    ${CMAKE_BINARY_DIR}/bin/xscale
 )
 
 foreach(file ${cmake_generated})
   if (EXISTS ${file})
      file(REMOVE_RECURSE ${file})
-  else (NOT EXISTS ${file})
-     message("File ${file} not found")
   endif()
 
 endforeach(file)
