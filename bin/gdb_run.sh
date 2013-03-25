13151664af99804c59ec8d06cd0b150bf31f1c3c
Keidan <kuso.fr@gmail.com>
2013-03-25 18:56:19 +0100

    Update run with arch param

--- bin/gdb_run.sh
+++ bin/gdb_run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+ARCH="${1}"
+if [[ -z "${ARCH}" ]]; then
+  echo "Arch binary required!"
+  exit 1
+fi
+if [[ ! -f "${ARCH}/bns" ]]; then
+  echo "${ARCH}/bns not found!"
+  exit 1
+fi
+shift
+cd "${ARCH}"
+
 OTHER=""
-if [ ! -z "${LD_LIBRARY_PATH}" ]; then
+if [[ ! -z "${LD_LIBRARY_PATH}" ]]; then
     OTHER=":${LD_LIBRARY_PATH}"
 fi
 export LD_LIBRARY_PATH=.${OTHER}
