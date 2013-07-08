77ab59bbac8ebe801a273eaee71630f5151b7554
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2013-07-08 20:57:26 +0200

    update tk library

--- cmake/tk.cmake
+++ cmake/tk.cmake
@@ -17,13 +17,16 @@ add_dependencies(bns libtk)
 add_dependencies(clean-all libtk_clean-all)
 set(extra_clean 
   lib/tk/lib/${DISTRIBUTION}/${CMAKE_BUILD_TYPE}/libtk.so 
-  lib/tk/src/CMakeFiles/tk.dir/text/string.c.o
-  lib/tk/src/CMakeFiles/tk.dir/text/stringtoken.c.o
-  lib/tk/src/CMakeFiles/tk.dir/sys/log.c.o
-  lib/tk/src/CMakeFiles/tk.dir/sys/sysutils.c.o
-  lib/tk/src/CMakeFiles/tk.dir/sys/proc.c.o
-  lib/tk/src/CMakeFiles/tk.dir/net/netutils.c.o
-  lib/tk/src/CMakeFiles/tk.dir/net/netprint.c.o
+  lib/tk/src/tk/CMakeFiles/tk.dir/text/string.c.o
+  lib/tk/src/tk/CMakeFiles/tk.dir/text/stringtoken.c.o
+  lib/tk/src/tk/CMakeFiles/tk.dir/sys/log.c.o
+  lib/tk/src/tk/CMakeFiles/tk.dir/sys/sysutils.c.o
+  lib/tk/src/tk/CMakeFiles/tk.dir/sys/uz.c.o
+  lib/tk/src/tk/CMakeFiles/tk.dir/sys/proc.c.o
+  lib/tk/src/tk/CMakeFiles/tk.dir/net/netutils.c.o
+  lib/tk/src/tk/CMakeFiles/tk.dir/net/netprint.c.o
+  lib/tk/src/zlib-minizip/CMakeFiles/zlib-minizip.dir/unzip.c.o
+  lib/tk/src/zlib-minizip/CMakeFiles/zlib-minizip.dir/ioapi.c.o
 )
 set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${extra_clean}")
 
